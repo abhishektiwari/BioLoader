b0a728369f62a10a7c8d0b1eeaec8c8e904d632d
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2010-10-19 18:07:42 +1300

    Modified the setup.py, added  files in bin, data, examples and doc folders

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 
-for False in list:
-	hfh
-    fff
 
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+ 
+config = {
+	'description': 'BioLoader',
+	'author': 'Abhishek Tiwari',
+	'url': 'http://github.com/abhishektiwari/BioLoader',
+	'download_url': 'http://github.com/abhishektiwari/BioLoader',
+	'author_email': 'abhishek@abhishek-tiwari.com',
+	'version': '0.1',
+	'install_requires': ['tweepy','ftputil'],
+	'packages': ['bioloader', 'bioftp', 'bioftpalerts'],
+	'scripts': [],
+}
+
+setup(**config)
